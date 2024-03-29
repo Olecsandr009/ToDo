7c3d55efd840cc970537e61b787c1da42a6d1b44
Olecsandr009 <sasha2005vit@gmail.com>
2024-03-29 19:13:16 +0200

    update for module logout

--- js/modules/authentication/logout.js
+++ js/modules/authentication/logout.js
@@ -9,37 +9,35 @@ import { closeBurgers } from "../../utils/burger.js"
 const logout = document.querySelector("[data-logout]")
 const logoutBtn = document.querySelector("[data-logout-buttons]")
 
-if(logout) {
-    logout.addEventListener("click", e => {
-        e.preventDefault()
+if(logout) logout.addEventListener("click", e => {
+    e.preventDefault()
 
-        allClose()
-        closeBurgers()
-        openPopup(popups['logout'])
-    })
-}
+    allClose()
+    closeBurgers()
+    
+    openPopup(popups['logout'])
+})
 
-if(logoutBtn) {
-    logoutBtn.addEventListener('click', async e => {
+if(logoutBtn) logoutBtn.addEventListener('click', async e => {
+    try {
         e.preventDefault()
-        let status = false
 
         if(e.target.closest("[data-false]")) {
-            status = false
-            
             allClose()
             closeBurgers()
         }
 
         if(e.target.closest("[data-true]")) {
-            status = await googleSingOut()
-        }
-
-        if(status) {
+            await googleSingOut()
+            
             allClose()
             closeBurgers()
+
             openAlert(alerts["logout"])
             getUserGoogle()
         }
-    })
-}
+
+    } catch(error) {
+        console.log(error.message)
+    }
+})
